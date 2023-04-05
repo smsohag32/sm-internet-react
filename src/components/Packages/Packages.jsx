22cdb14fcb243f536f578f49ac04be5d25343505
SM Sohag <sohagsheik32@gmail.com>
2023-04-05 23:34:28 +0600

    initial

---
+++ src/components/Packages/Packages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Packages = () => {
+  return <div></div>;
+};
+
+export default Packages;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Home from "./components/Home/Home";
+import Packages from "./components/Packages/Packages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "packages",
+        element: <Packages />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode></React.StrictMode>
+);
